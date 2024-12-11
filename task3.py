7f6fc702abc6ca269f68cc72aad0a0154901e620
Kolya1800 <89708524+Kolya1800@users.noreply.github.com>
2024-12-10 21:05:00 -0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import logging
 import shutil
+import re
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -29,16 +30,37 @@ def organize_files(directory):
             logger.info(f"Moved .{file_extension} files to {target_dir}")
     logger.info("Directory organization complete.")
 
+def monitor_log(log_file):
+    logger.info(f"Monitoring log file {log_file}")
+    if not os.path.exists(log_file):
+        logger.error(f"Log file '{log_file}' does not exist.")
+        return
+    with open(log_file, 'r') as file:
+        for line in file:
+            match = re.search(r'error|warning|critical', line, re.IGNORECASE)
+            if match:
+                logger.warning(f"Found {match.group()} in log file: {line.strip()}")
+    logger.info("Log file monitoring complete.")
+
 def main():
     parser = argparse.ArgumentParser(description='File Organizer Script')
-    parser.add_argument('--dir', type=str, help='Directory to organize files in')
+    subparsers = parser.add_subparsers(dest='command')
+
+    organize_parser = subparsers.add_parser('organize')
+    organize_parser.add_argument('--dir', type=str, help='Directory to organize files in')
+
+    monitor_parser = subparsers.add_parser('organize')
+    monitor_parser.add_argument('--log-monitor', type=str, help='Log file to monitor')
 
     args = parser.parse_args()
 
-    if args.dir:
-        organize_files(args.dir)
-    else:
-        logger.error("Please specify a directory to organize files in.")
+    if args.command == 'organize':
+        if args.dir:
+            organize_files(args.dir)
+        elif args.log_monitor:
+            monitor_log(args.log_monitor)
+        else:
+            logger.error("Please specify a directory to organize files in or a log file to monitor.")
 
 if __name__ == '__main__':
     main()
